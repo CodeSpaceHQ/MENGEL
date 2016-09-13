155667b2600bb1c9728ba67774e214f5fca9f39c
asclines <injpk23@gmail.com>
2016-09-13 10:39:08 -0500

    Setup input validation for test score

--- logger/logger.sh
+++ logger/logger.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
 
 PROGRAM_NAME=$0
-KEYS=""
-VALUES=""
 SCORE=-1
 LABEL=""
+MODELCOUNT=1
+CURMODEL=0
+
+KEYS=""
+VALUES=""
+
 
 # Since there are several situatoins where usage instructions will be needed to
 # be shown to the user, it makes sense to pull that out into it's own function
 function help {
-  echo "usage: $PROGRAM_NAME -s score [-h] [-m model] [-l label]"
-  echo "  -s score  Score from results, must be greater than 0"
-  echo "  -h        Displays these usage instructions"
-  echo "  -m model  Pass in a key value pair for the model, must be in format key:value"
-  echo "  -l label  Label that can be used to identify this test later"
+  echo "usage: $PROGRAM_NAME -s score [-h] [-n modelcount] [-m model] [-l label]"
+  echo "  -s score      Score from results, must be an integer in [0,100]"
+  echo "  -n modelcount Number of models being submitted. (Defaults to 1)"
+  echo "  -h            Displays these usage instructions"
+  echo "  -m model      Pass in a key value pair for the model, must be in format key1:value1:key2:value2:"
+  echo "  -l label      Label that can be used to identify this test later"
   exit 1
 }
 
 # Parameters
-# - $1 : A key value pair in the fomat KEY:VALUE
+# - $1 : All the key value pairs for a particular model in the format
+# key1:value1:key2:value2...
 function addKeyValuePair {
   IFS=':' read -ra kvpArr <<< "$1"
-  kvpKey=${kvpArr[0]}
-  kvpValue=${kvpArr[1]}
-  KEYS="$KEYS:$kvpKey"
-  VALUES="$VALUES:$kvpValue"
+  kvpCount="${#kvpArr[*]}"
+  for ((index=0; index<"$kvpCount"; index=index+2));
+    do
+      kvpKey=${kvpArr[index]}
+      kvpValue=${kvpArr[(index+1)]}
+
+      KEYS="$KEYS:$kvpKey"
+      VALUES="$VALUES:$kvpValue"
+    done
+
+
+
+  echo "Keys read in"
+  echo "Keys = $KEYS"
+  echo "Values = $VALUES"
 }
 
 
@@ -33,8 +50,25 @@ while getopts ":hs:m:l:" opt; do
     h)
       help
       ;;
+    n)
+      MODELCOUNT=$OPTARG
+      ;;
     s)
       SCORE=$OPTARG
+      re='^[0-9]+$'
+      if ! [[ $SCORE =~ $re ]] ; then
+         echo "Error: Score was not a positive integer." >&2;
+         help
+         exit 1
+      elif [[ $SCORE -lt 0 ]]; then
+          echo "Error: Score was less than 0" >&2
+          help
+          exit 1
+      elif [[ $SCORE -gt 100 ]]; then
+        echo "Error Score was greater than 100" >&2
+        help
+        exit 1
+      fi
       ;;
     m)
       addKeyValuePair $OPTARG
@@ -75,7 +109,6 @@ let LASTINDEX=ARRSIZE-2
 
 #index starts at 1 because the first one is always going to be empty
 for ((index=1; index<"$ARRSIZE"; ++index));
-
 do
   JSONLINE="\"${KEYARR[index]}\" : \"${VALARR[index]}\""
   if [ "$index" -eq "$LASTINDEX" ]
