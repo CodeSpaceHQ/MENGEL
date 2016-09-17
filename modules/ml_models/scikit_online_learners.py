0220c5152dfbac0286d21c96ea5612b8fa52fcfc
Isaac Griswold-Steiner <isaac31415926@gmail.com>
2016-09-16 21:05:47 -0500

    Setting up and demoing online learning models

---
+++ modules/ml_models/scikit_online_learners.py
@@ -0,0 +1,41 @@
+import sys
+import csv
+
+import numpy as np
+import pandas as pd
+from pandas import read_csv, DataFrame
+from sklearn import linear_model
+from sklearn import cross_validation
+from sklearn import preprocessing
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+
+sys.path.insert(0, '../../datasets')
+
+def run_SGDRegressor(path, filename):
+    pandas_data = pd.read_csv(path + filename, sep=';')
+    numpy_data = pd.DataFrame.as_matrix(pandas_data)
+
+    # Selection of training/target data and scaling of the training data.
+    # Scaling is important because if the variables are too different from
+    # one another, it can throw off the model.
+    # EX: If one variable has an average of 1000, and another has an average
+    # of .5, then the model won't be as accurate.
+    y = numpy_data[:,11]
+    X = numpy_data[:,0:11]
+    X = preprocessing.scale(X)
+
+    # Feature Selection, didn't help
+    # X = SelectKBest(f_regression, k = 8).fit_transform(X, y)
+
+    # Selecting training and test sets
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2, random_state = 123123)
+
+    # Picking model
+    SGD_model = linear_model.SGDRegressor()
+
+    # Training the model. "partial_fit" can be used to train the model one chunk of data at a time.
+    SGD_model.fit(X_train, y_train)
+
+    # Scores the model using the coefficient of determination R^2 of the prediction.
+    print(SGD_model.score(X_test, y_test))
--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@ import sys
 import os
 
 sys.path.insert(0, '/modules/toolbox')
+sys.path.insert(0, '/modules/ml_models')
 
-import csv_splitter
+import modules.ml_models.scikit_online_learners as online_ml
+import modules.toolbox.csv_splitter as csv_split
 
 def csv_setup():
-    script_dir = os.path.dirname(__file__)
-    total_file_path = script_dir + '/data/'
-    
-    csv_splitter.setup_split(total_file_path, 'train_categorical.csv')
-    csv_splitter.setup_split(total_file_path, 'train_date.csv')
-    csv_splitter.setup_split(total_file_path, 'train_numeric.csv')
+    script_dir = os.getcwd() # Compatible with Linux and Windows
+    total_file_path = script_dir + '/datasets/'
+
+    csv_split.setup_split(total_file_path, 'train_categorical.csv')
+    csv_split.setup_split(total_file_path, 'train_date.csv')
+    csv_split.setup_split(total_file_path, 'train_numeric.csv')
 
     open(total_file_path + 'split.txt', 'w')
 
-csv_setup()
+def test_online_learners():
+    script_dir = os.getcwd() # Compatible with Linux and Windows
+    total_file_path = script_dir + '/datasets/'
+
+    online_ml.run_SGDRegressor(total_file_path, "winequality-red.csv")
+
+test_online_learners()
