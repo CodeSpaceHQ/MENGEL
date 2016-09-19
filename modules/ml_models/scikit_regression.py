e5cb73933936bc24de38e8bde9c39dacf16065dd
Isaac Griswold-Steiner <isaac31415926@gmail.com>
2016-09-18 22:33:33 -0500

    Adding more models
    Not being very picky, just adding a bunch of options and removing the
    old csv_splitter. Hopefully the non-online ones will still be usable,
    but that might take some tinkering or a more powerful machine.

--- modules/ml_models/scikit_regression.py
+++ modules/ml_models/scikit_regression.py
@@ -1,19 +1,15 @@
 from sklearn import linear_model
+from sklearn import svm
 from modules.toolbox import framework_tools as ft
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
 
-#
-# I apologize for my variable and function names, will fix in the future (to Alex)
-# ^^ If that doesn't happen I owe you 50 bucks
-#
-# There is also some repetitive code in terms of fitting and scoring models, this will
-# likely be split out into a new helper
-#
 
-def run_SGDRegressor(path, filename):
+def run_BayesianRidge(path, filename):
     X_train, X_test, y_train, y_test = ft.get_TrainTest(path, filename)
 
     # Picking model
-    model = linear_model.SGDRegressor()
+    model = linear_model.BayesianRidge()
 
     # Training the model. "partial_fit" can be used to train the model one chunk of data at a time.
     model.fit(X_train, y_train)
@@ -22,11 +18,11 @@ def run_SGDRegressor(path, filename):
     return model.score(X_test, y_test)
 
 
-def run_PassiveAggressiveRegressor(path, filename):
+def run_SupportVectorRegression(path, filename):
     X_train, X_test, y_train, y_test = ft.get_TrainTest(path, filename)
 
     # Picking model
-    model = linear_model.PassiveAggressiveRegressor()
+    model = svm.SVR()
 
     # Training the model. "partial_fit" can be used to train the model one chunk of data at a time.
     model.fit(X_train, y_train)
@@ -34,3 +30,15 @@ def run_PassiveAggressiveRegressor(path, filename):
     # Scores the model using the coefficient of determination R^2 of the prediction.
     return model.score(X_test, y_test)
 
+
+def run_AdaBoostDTR(path, filename):
+    X_train, X_test, y_train, y_test = ft.get_TrainTest(path, filename)
+
+    # Picking model
+    model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), n_estimators=300)
+
+    # Training the model. "partial_fit" can be used to train the model one chunk of data at a time.
+    model.fit(X_train, y_train)
+
+    # Scores the model using the coefficient of determination R^2 of the prediction.
+    return model.score(X_test, y_test)
--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 import os
 import inspect
 
-import modules.ml_models.scikit_online_learners as online_ml
-import modules.toolbox.csv_splitter as csv_split
 
-def csv_setup():
-    total_file_path = get_datasets_path()
-
-    csv_split.setup_split(total_file_path, 'train_categorical.csv')
-    csv_split.setup_split(total_file_path, 'train_date.csv')
-    csv_split.setup_split(total_file_path, 'train_numeric.csv')
-
-    open(total_file_path + 'split.txt', 'w')
-
+# Maybe move to framework_tools
 def get_datasets_path():
     script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     return script_dir + '/datasets/'
