cfe599feb3fced872ae0f96c10b234320f2ba192
Zakery Fyke <zak7062@gmail.com>
2016-09-19 10:58:53 -0500

    testing update
    added additional tests for models

--- tests/test_online_learning.py
+++ tests/test_online_learning.py
@@ -1,4 +1,3 @@
-# Ugly hack
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -9,4 +8,8 @@ import setup
 class TestOnlineLearning(TestCase):
     def test_sgd_regressor(self):
         result = scikit_online_learners.run_sgd_regressor(setup.get_datasets_path(), "winequality-red.csv")
+        self.assertGreater(result, 0, msg="Failed to beat baseline")
+    
+    def test_passive_aggressive_regressor(self):
+        result = scikit_online_learners.run_passive_aggressive_regressor(setup.get_datasets_path(), "winequality-red.csv")
         self.assertGreater(result, 0, msg="Failed to beat baseline")
