dfd242909c919f8af1cc435bdaaf2396e728cdbe
Zak7062 <zak7062@gmail.com>
2016-10-01 18:18:28 -0500

    Additional Reduction Methods
    Added ICA Reducer, Gaussian Random Projection Reducer, and Factor
    Analysis reducer.
    Still need to find way to test results of reduction methods.
    Ugly code for my own testing left in.

--- reducers/scikit_dimensionality_reducers.py
+++ reducers/scikit_dimensionality_reducers.py
@@ -5,28 +5,40 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from modules.toolbox import framework_tools as ft
 from modules.toolbox import ml_runners as mr
-from modules.ml_models import scikit_classification_learners
 import setup
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.random_projection import GaussianRandomProjection
 from sklearn.decomposition import PCA
+from sklearn.decomposition import FactorAnalysis
+from sklearn.decomposition import FastICA
 
 #http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-def PCA_reducer():
-    data = ft.get_data(setup.get_datasets_path(), "titanic_train.csv", ',')
-    print(data.head)
-    data_df = pd.DataFrame(data)
-    pca = PCA(n_components=5) #if components is not specified, all components are kept.
-    pca.fit_transform(data_df) #This both fits pca and transforms in 1 command
-    x = (pd.DataFrame(pca.components_, columns=data_df.columns, index=['PC-1', 'PC-2', 'PC-3', 'PC-4', 'PC-5']))
-    return x
+def PCA_reducer(data):
+    pca = PCA(n_components=3) #if components is not specified, all components are kept.
+    pca.fit_transform(data) #This both fits pca and transforms in 1 command
+    pca = (pd.DataFrame(pca.components_, columns=data.columns)) #Retain feature labels
+    return pca
 
-x = PCA_reducer()
+#http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.FastICA.html#sklearn.decomposition.FastICA
+def ICA_reducer(data):
+    ica = FastICA(n_components=3)
+    ica.fit_transform(data)
+    ica = (pd.DataFrame(ica.components_, columns=data.columns))
+    return ica
+
+#http://scikit-learn.org/stable/modules/generated/sklearn.random_projection.GaussianRandomProjection.html#sklearn.random_projection.GaussianRandomProjection
+def GaussianRandomProjection_reducer(data):
+    grp = GaussianRandomProjection(n_components = 3) #default is 'auto', but leads to interesting results
+    grp.fit_transform(data)
+    grp = (pd.DataFrame(grp.components_, columns=data.columns))
+    return grp
+
+def FactorAnalysis_reducer(data):
+    far = FactorAnalysis(n_components = 3)
+    far.fit_transform(data)
+    far = (pd.DataFrame(far.components_, columns=data.columns))
+    return far
+
+
+data = ft.get_data(setup.get_datasets_path(), "titanic_train.csv", ',')
+x = ICA_reducer(data)
 print(x)
-# def train_knn(data):
-#     x_train, x_test, y_train, y_test = ft.get_train_test(data, "Survived")
-#
-#     # Selecting the model
-#     model = KNeighborsClassifier(n_neighbors=5)
-#     model.fit(x_train, y_train)
-#     # Training the model.
-#     return model.score(x_test, y_test)
