55d4d5aeb6afb2db05dd296161cad894cbbf01e8
asclines <injpk23@gmail.com>
2016-10-04 10:29:37 -0500

    Setup testing for firebase manager including a self-contained local server. Added testing for connecting to Firebase

--- modules/MLTA/mlta/firebase-manager.js
+++ modules/MLTA/mlta/firebase-manager.js
@@ -1,70 +1,77 @@
 #!/usr/bin/env node
+
 var firebase = require("firebase"); //For Firebase stuff..duh..
 
 var fbMainDir = '/mlta';
-var fbResultDir = fbMainDir+'/results/'
+var fbResultDir = fbMainDir + '/results/'
 
 var exports = module.exports = {};
 
 //Handles initializing firebase and checking authentication.
-exports.connectToFirebase = function(mltaConfig,cb){
-  var fbConfig = {
-    serviceAccount: mltaConfig.serviceAccount,
-    databaseURL: mltaConfig.databaseURL
-  };
-  firebase.initializeApp(fbConfig);
-   var db = firebase.database();
+exports.connectToFirebase = function(mltaConfig, done) {
+    var fbConfig = {
+        serviceAccount: mltaConfig.serviceAccount,
+        databaseURL: mltaConfig.databaseURL,
+        authDomain: mltaConfig.authDomain
+    };
 
-   //If we don't have a connection in a few seconds, whether its due to incorrect credintials, or network error, we cannot continue.
-   var connFailTimeout = setTimeout(function() {
-     var error = new Error('Failed to connect to Firebase.');
-     cb(errow);
-   }, 10000);
+    firebase.initializeApp(fbConfig);
 
-   //Called if we have an established, authorized connection to the Firebase database
-  function ready() {
-    clearTimeout(connFailTimeout); //We've connected to lets go ahead and tell the connFailTimeout dude up there ^ that he can leave his post.
-    cb(null);
+    var db = firebase.database();
 
+    //If we don't have a connection in a few seconds, whether its due to incorrect credintials, or network error, we cannot continue.
+    var connFailTimeout = setTimeout(function() {
+        var error = new Error('Failed to connect to Firebase.');
+        done(error);
+    }, 10000);
 
-  };
+    //Called if we have an established, authorized connection to the Firebase database
+    function ready() {
+        clearTimeout(connFailTimeout); //We've connected to lets go ahead and tell the connFailTimeout dude up there ^ that he can leave his post.
+        done(null);
+    };
 
-  /*
-    The connection function
-    meets at the internet junction
-    to make a conjunction
-    or an adjunction
-    unless theres an injunction
-    to which we disfunction
-    and ends with expunction.
-  */
-  var connFunc = db.ref('.info/connected').on('value', function(s) {
-    if(s.val() === true) {
-      db.ref('.info/connected').off('value', connFunc);
+    //The way the mock firebase-server currently works, the database is offline so we have to do it this way
+    if(mltaConfig.name == "MLTA-Test"){
       ready();
     }
-  });
+    /*
+      The connection function
+      meets at the internet junction
+      to make a conjunction
+      or an adjunction
+      unless theres an injunction
+      to which we disfunction
+      and ends with expunction.
+    */
+    var connFunc = db.ref('.info/connected').on('value', function(s) {
+        if (s.val() === true) {
+            console.log("Ready!");
+            db.ref('.info/connected').off('value', connFunc);
+            ready();
+        }
+    });
 
 }
 
 
 
 
-exports.saveResult = function(result,cb){
-  // Get a key for a new Post.
-  var resultKey = firebase.database().ref().child(fbResultDir).push().key;
+exports.saveResult = function(result, done) {
+    // Get a key for a new Post.
+    var resultKey = firebase.database().ref().child(fbResultDir).push().key;
 
-  var updates = {};
-  updates[fbResultDir + resultKey] = result;
+    var updates = {};
+    updates[fbResultDir + resultKey] = result;
 
-  var fbp = firebase.database().ref().update(updates);
-  fbp.then(
-    function(val){
-      cb(null);
-    }
-  ).catch(
-    function(reason){
-      cb(reason);
-    }
-  )
+    var fbp = firebase.database().ref().update(updates);
+    fbp.then(
+        function(val) {
+            done(null);
+        }
+    ).catch(
+        function(reason) {
+            done(reason);
+        }
+    )
 }
