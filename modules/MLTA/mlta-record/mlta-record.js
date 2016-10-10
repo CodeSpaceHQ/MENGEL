d96574765cdf987d6e4125f40e5532427fea61c5
asclines <injpk23@gmail.com>
2016-10-10 07:29:09 -0500

    Self-Code review/cleanup of MLTA/mlta-record: - Styled code
    Follow ups:
    - None

--- modules/MLTA/mlta-record/mlta-record.js
+++ modules/MLTA/mlta-record/mlta-record.js
@@ -9,22 +9,17 @@ var path = require('path'); //Handles path naming
 var tracer = require('tracer');
 tracer.setLevel(4) //'log':0, 'trace':1, 'debug':2, 'info':3, 'warn':4, 'error':5
 var logger = tracer.console({
-    format : "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
-    dateformat : "HH:MM:ss.L"
-  });
+    format: "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
+    dateformat: "HH:MM:ss.L"
+});
 
 
 var fb = require('../mlta/firebase-manager');
 
-//Helper function for logging
-function log(message) {
-    console.log(message);
-}
-
 //Helper function for handling errors
-function onError(err){
-  logger.error('Stack: %j', err);
-  return 1;
+function onError(err) {
+    logger.error('Stack: %j', err);
+    return 1;
 }
 
 function modelData(val, data) {
@@ -43,73 +38,73 @@ function testData(val, data) {
 }
 
 //Get arguemnts/options
-function validateOptions(program, done){
-  var options = new Object();
-  //Make sure all required arguments have been met and that all optional arguemnts that were not passed in have their default values set
-  if (!program.project) {
-    return done(new Error('Project name must be specified.'));
-  }
-  options.project = program.project;
+function validateOptions(program, done) {
+    var options = new Object();
+    //Make sure all required arguments have been met and that all optional arguemnts that were not passed in have their default values set
+    if(!program.project) {
+        return done(new Error('Project name must be specified.'));
+    }
+    options.project = program.project;
 
-  if (!program.label) {
-    logger.info('No label added.');
-    options.label = '';
-  } else {
-    options.label = program.label;
-  }
+    if(!program.label) {
+        logger.info('No label added.');
+        options.label = '';
+    } else {
+        options.label = program.label;
+    }
 
-  if (!program.modelType) {
-    return done(new Error('Model type must be specified.'));
-  }
-  options.modelType = program.modelType;
+    if(!program.modelType) {
+        return done(new Error('Model type must be specified.'));
+    }
+    options.modelType = program.modelType;
 
-  options.modelData = program.modelData;
-  options.testData = program.testData;
+    options.modelData = program.modelData;
+    options.testData = program.testData;
 
-  done(null,options);
+    done(null, options);
 }
 
 
-function saveRecordToFB(options,done){
+function saveRecordToFB(options, done) {
 
-  logger.debug('Options: %j', options);
-  var configFileDir = path.join(process.env.HOME, '.mlta', options.project);
-  var configFile = configFileDir + '.config';
-  fs.access(configFile, fs.F_OK, function(err) {
-      if (err) {
-          log("Error: Could not load config file for " + options.project);
-          return done(err);
-      }
-
-      var config = jsonfile.readFileSync(configFile.toString());
-      logger.debug('Config: %j',config);
-
-      fb.connectToFirebase(config, function(err) {
-          if (err) {
+    logger.debug('Options: %j', options);
+    var configFileDir = path.join(process.env.HOME, '.mlta', options.project);
+    var configFile = configFileDir + '.config';
+    fs.access(configFile, fs.F_OK, function(err) {
+        if(err) {
+            log("Error: Could not load config file for " + options.project);
             return done(err);
-          }
-          logger.info('Connected to Firebase!');
+        }
 
-          var result = {
-              label: options.label,
-              createdAt: new Date().toJSON(),
-              author: config.author,
-              modelType: options.modelType,
-              modelData: options.modelData,
-              testData: options.testData
+        var config = jsonfile.readFileSync(configFile.toString());
+        logger.debug('Config: %j', config);
 
-          };
-
-          fb.saveResult(result, function(err) {
-              if (err) {
+        fb.connectToFirebase(config, function(err) {
+            if(err) {
                 return done(err);
-              } else {
-                return done(null);
-              }
-          });
-      });
+            }
+            logger.info('Connected to Firebase!');
 
-  });
+            var result = {
+                label: options.label,
+                createdAt: new Date().toJSON(),
+                author: config.author,
+                modelType: options.modelType,
+                modelData: options.modelData,
+                testData: options.testData
+
+            };
+
+            fb.saveResult(result, function(err) {
+                if(err) {
+                    return done(err);
+                } else {
+                    return done(null);
+                }
+            });
+        });
+
+    });
 }
 
 program
@@ -122,15 +117,15 @@ program
     .parse(process.argv);
 
 
-  validateOptions(program, function(err,options){
-    if(err){
-      return onError(err);
+validateOptions(program, function(err, options) {
+    if(err) {
+        return onError(err);
     }
     logger.info('onValidOptionsDone');
-    logger.debug('Options: %j',options);
-    saveRecordToFB(options,function(err){
-      logger.info('saved to firebase');
-      if(err) return onError(err);
-      process.exit();
+    logger.debug('Options: %j', options);
+    saveRecordToFB(options, function(err) {
+        logger.info('saved to firebase');
+        if(err) return onError(err);
+        process.exit();
     });
-  });
+});
