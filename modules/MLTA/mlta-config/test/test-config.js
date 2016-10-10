143323a7d935886661fa6d4f9e70f7fc45dfaa7e
asclines <injpk23@gmail.com>
2016-10-10 07:26:37 -0500

    Self-Code review/cleanup of MLTA/mlta-config: - Styled Code
    Follow ups:
    - The way the mock objects are done in testing seems a little messy. Look into a better way to handle that.

--- modules/MLTA/mlta-config/test/test-config.js
+++ modules/MLTA/mlta-config/test/test-config.js
@@ -5,78 +5,78 @@ var stdin = require('mock-stdin').stdin();
 //Logging stuff
 var logger = require('tracer').console({
     level: 'warn',
-    format : "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
-    dateformat : "HH:MM:ss.L"
-  });
+    format: "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
+    dateformat: "HH:MM:ss.L"
+});
 
 var testConfigObj = {
-  name: 'TestName',
-  author: 'TestAuthor',
-  databaseURL: 'DBURL',
-  serviceAccount: 'ServiceAccount'
+    name: 'TestName',
+    author: 'TestAuthor',
+    databaseURL: 'DBURL',
+    serviceAccount: 'ServiceAccount'
 }
 
 var resultMockObj = {
-  author: testConfigObj.author,
-  db: testConfigObj.databaseURL,
-  sa: testConfigObj.serviceAccount
+    author: testConfigObj.author,
+    db: testConfigObj.databaseURL,
+    sa: testConfigObj.serviceAccount
 }
 
 var promptMock = {
-  get: function(properties,cb){
-    logger.info("PromptMock");
-    cb(null,resultMockObj);
-  }
+    get: function(properties, cb) {
+        logger.info("PromptMock");
+        cb(null, resultMockObj);
+    }
 }
 
 var fsMock = {
-  accessSync: function(value,status){
-    logger.info('Using FS mock');
-    expect(value).to.equal(testConfigObj.serviceAccount);
-  }
+    accessSync: function(value, status) {
+        logger.info('Using FS mock');
+        expect(value).to.equal(testConfigObj.serviceAccount);
+    }
 }
 
 var firebaseManagerMock = {
-  connectToFirebase: function(config,cb){
-    logger.info('Using FireebaseManager mock');
-    cb(null);
-  }
+    connectToFirebase: function(config, cb) {
+        logger.info('Using FireebaseManager mock');
+        cb(null);
+    }
 }
 
 
 var mc = rewire('../mlta-config.js');
 
 mc.__set__({
-  fs: fsMock,
-  prompt: promptMock,
-  fb: firebaseManagerMock
+    fs: fsMock,
+    prompt: promptMock,
+    fb: firebaseManagerMock
 });
 
-describe('config',function(){
-  describe('#createNewConfig(name,configFile)',function(){
-    it('should create a new config file.',function(done){
-      var configManagerMock = {
-          saveConfig: function(config,cb){
-            logger.info('mock cm.saveConfig')
-            expect(config.name).to.equal(testConfigObj.name)
-            expect(config.author).to.equal(testConfigObj.author)
-            expect(config.databaseURL).to.equal(testConfigObj.databaseURL)
-            expect(config.serviceAccount).to.equal(testConfigObj.serviceAccount)
-            cb(null)
-          }
-      }
-      mc.__set__({
-        cm: configManagerMock
-      })
-      createConfigMethod = mc.__get__('createNewConfig');
-      createConfigMethod(testConfigObj.name,new Object());
-      stdin.send([
+describe('config', function() {
+    describe('#createNewConfig(name,configFile)', function() {
+        it('should create a new config file.', function(done) {
+            var configManagerMock = {
+                saveConfig: function(config, cb) {
+                    logger.info('mock cm.saveConfig')
+                    expect(config.name).to.equal(testConfigObj.name)
+                    expect(config.author).to.equal(testConfigObj.author)
+                    expect(config.databaseURL).to.equal(testConfigObj.databaseURL)
+                    expect(config.serviceAccount).to.equal(testConfigObj.serviceAccount)
+                    cb(null)
+                }
+            }
+            mc.__set__({
+                cm: configManagerMock
+            })
+            createConfigMethod = mc.__get__('createNewConfig');
+            createConfigMethod(testConfigObj.name, new Object());
+            stdin.send([
         testConfigObj.name,
         testConfigObj.author,
         testConfigObj.databaseURL,
         testConfigObj.serviceAccount
       ]);
-      done();
+            done();
+        })
     })
-  })
 })
