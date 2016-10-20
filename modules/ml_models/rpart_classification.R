2520b90c643057dcdb6bf99ee412ddd37cc572a4
Ryan Berg <ryanmcberg@gmail.com>
2016-10-19 20:27:25 -0500

    initial commit

---
+++ modules/ml_models/rpart_classification.R
@@ -0,0 +1,11 @@
+#####   rpart_classification.R
+#
+#
+
+
+###  DOCUMENTATION
+##   this module requires the rpart package be install.
+#    to install package type install.packages("rpart") in R console
+
+
+library(rpart)
---
+++ modules/ml_models/rpart_classification.py
@@ -0,0 +1,16 @@
+"""
+Ryan Berg 10/19/16
+
+User Story -  As a User I want to be able to run a dataset agains and Rpart model
+
+"""
+
+
+import sys
+import subprocess
+import os
+
+class Rpart:
+
+    def __init__(self):
+        return
