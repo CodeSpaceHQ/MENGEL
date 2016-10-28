f80e5a757554f28d1001c527b6d4c7ac14a3766a
Zakery Fyke <zak7062@gmail.com>
2016-10-28 10:09:08 -0500

    Removed separator variable
    Removed separator variable since it is autodetected

--- modules/DMZ/utils/config_options.py
+++ modules/DMZ/utils/config_options.py
@@ -4,7 +4,6 @@ class ConfigOptions(object):
     def __init__(self):
         self.training_file_name = None
         self.test_file_name = None
-        self.separator_type = None
         self.prediction_type = None
         self.target_column = None
         self.get_config_options()
@@ -14,7 +13,6 @@ class ConfigOptions(object):
     def get_config_options(self):
         self.training_file_name = raw_input("Provide the training file name: ")
         self.test_file_name = raw_input("Provide the testing file name: ")
-        self.separator_type = raw_input("Provide the separator for the data files: ")
         self.prediction_type = raw_input("Provide the type of prediction being done (Auto, Regression, or Classification: ")
         self.target_column = raw_input("Provide the name of the column to be predicted: ")
 
