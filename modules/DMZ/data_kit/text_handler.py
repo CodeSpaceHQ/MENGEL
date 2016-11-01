b4383a9fb2f3c8fac0cbbfde74f27c4d96195696
Zak7062 <zak7062@gmail.com>
2016-11-01 18:59:32 -0500

    minor edits
    minor formatting. removed test casey stuff at bottom

--- modules/DMZ/data_kit/text_handler.py
+++ modules/DMZ/data_kit/text_handler.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+
 # Takes a given column of a pandas dataframe and returns the column with all
 # text values changed to categorical numeric values
 def text_column_to_numeric(column):
@@ -10,25 +11,19 @@ def text_column_to_numeric(column):
     column = le.transform(column)
     return column
 
-def convert_dataframe(pandas_data, n):
-    #For every column, check if the column is text. If so, and it has fewer than n unique values, convert it.
+
+# Iterates over every column of the given dataframe and checks if the column contains text. If so, and if it has fewer
+# than n unique values, the column will be converted to categorical numeric values.
+def convert_dataframe_text(pandas_data, n):
     for column in pandas_data:
-        if(pandas_data[column].dtype == object):
+        if pandas_data[column].dtype == object:
             uniques = set(pandas_data[column])
-            if(len(uniques) < n):
+            if len(uniques) < n:
                 pandas_data[column] = text_column_to_numeric(pandas_data[column])
     return pandas_data
 
 
-
-
-
-x = pd.read_csv("titanic_train.csv", sep = ',')
-print(x)
-
-x = convert_dataframe(x,10)
-#print(x)
-# le=preprocessing.LabelEncoder()
-# le.fit(x["Sex"])
-# x["Sex"] = le.transform(x["Sex"])
-# print(x)
+# Converts text columns left over from convert_dataframe to numpy nans for later removal
+def convert_nonpredictive_text(pandas_data):
+    pandas_data = pandas_data.apply(pd.to_numeric, errors='coerce')
+    return pandas_data
