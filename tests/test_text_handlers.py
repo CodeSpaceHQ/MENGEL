1e5ee097d1eb47b4a2cedbccfc7df0ffff669e67
Zak7062 <zak7062@gmail.com>
2016-11-01 21:10:07 -0500

    Fixed pandas, added more tests
    Added test_convert_nonpredictive_text and text_convert_dataframe. Fixed
    pandas issue that wasn't allowing the use of to_numeric.

--- tests/test_text_handlers.py
+++ tests/test_text_handlers.py
@@ -7,22 +7,47 @@ from unittest import TestCase
 from modules.DMZ.data_kit import data_io
 from modules.DMZ.data_kit import text_handler
 import setup
-import numpy as np
-import pandas as pd
 
+import pandas as pd
+from sklearn import preprocessing
 from modules.toolbox.data_package import DataPackage
 from modules.toolbox.validation_package import ValidationPackage
 
 class TestDataFilling(TestCase):
 
     def test_text_column_to_numeric(self):
+
         #Arrange
         data = self.setup_data()
         column = data["Sex"]
+
         #Act
         data["Sex"] = text_handler.text_column_to_numeric(column)
+
         #Assert
-        self.assertTrue(data["Sex"].dtype != "object", msg="Failed to convert text")
+        self.assertTrue(data["Sex"].dtype != "object", msg="Failed to convert text to categorical values")
+
+    def test_convert_dataframe_text(self):
+
+        #Arrange
+        data = self.setup_data()
+
+        #Act
+        data = text_handler.convert_dataframe_text(data, 10)
+
+        #Assert
+        self.assertTrue(data["Sex"].dtype != "object" and data["Name"].dtype=="object", msg = "Converted incorrect features")
+
+    def test_convert_nonpredictive_text(self):
+
+        #Arrange
+        data = self.setup_data()
+
+        #Act
+        data = text_handler.convert_nonpredictive_text(data)
+
+        #Assert
+        self.assertTrue(data["Name"].isnull, msg = "Failed to convert text to NaN")
 
     def setup_data(self):
         data = data_io.get_data(setup.get_datasets_path(), "titanic_train.csv")
