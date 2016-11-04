212d02b2212bb621ef2b9972c2507eb01a3bbfa1
Zakery Fyke <zak7062@gmail.com>
2016-11-04 13:13:21 -0500

    updates to dataset_insight and data_filler
    put missing_ratios in dataset_insight into a single function instead of
    two separate functions. May separate the verification later.
    changed the data_filler ratio functions. May attempt to put into one
    function at later date, but probably not

--- modules/DMZ/data_kit/data_filler.py
+++ modules/DMZ/data_kit/data_filler.py
@@ -3,24 +3,25 @@ import numpy as np
 import dataset_insight
 
 
-# Drops rows which have fewer than threshold observations.
-# e.g. if threshold = 5, any rows with fewer than 5 observations will be removed from pandas_data.
-# def drop_missing_data_rows(pandas_data, threshold):
-#     pandas_data = pandas_data.dropna(thresh=threshold)
-#     return pandas_data
-#
-# def drop_missing_data_rows(pandas_data, desired_ratio):
-#
+#Drops rows which have a missing to existing component ratio greater than the desired_Ratio.
+#e.g. if desired_Ratio is .5, and a row is missing 60% of its components, the row will be dropped.
+def drop_missing_data_rows(pandas_data, desired_ratio):
+    ratios = dataset_insight.get_missing_ratios(pandas_data, "row")
+    ratios_dict = dict(zip(range(0, pandas_data.shape[0]), ratios))
+    for i in range(0, len(ratios)):
+        if ratios_dict[i] > desired_ratio:
+            pandas_data = pandas_data.drop(i, 0)
+    return pandas_data
 
 
 # Drops columns which have a missing to existing component ratio greater than the desired_ratio.
 # e.g. if desired_ratio is .5, and a column is missing 60% of its components, the column will be dropped.
 def drop_missing_data_columns(pandas_data, desired_ratio):
-    ratios = dataset_insight.get_missing_ratios(pandas_data)
-    for i in range(0, len(ratios)):
-        if ratios[i] > desired_ratio:
-            pandas_data = pandas_data.drop(pandas_data.columns[i], axis=1)
-
+    ratios = dataset_insight.get_missing_ratios(pandas_data, "column")
+    ratios_dict = dict(zip(pandas_data.columns.values, ratios))
+    for col in pandas_data:
+        if ratios_dict[col] > desired_ratio:
+            pandas_data = pandas_data.drop(col, 1)
     return pandas_data
 
 
@@ -48,10 +49,11 @@ def fill_missing_data_average(pandas_data):
     features = pandas_data.columns.values
     for feature in features:
         pandas_data[feature].fillna(pandas_data[feature].mean(), inplace=True)
-
     return pandas_data
 
 
-x = pd.read_csv("titanic_train.csv", sep=",")
-L = dataset_insight.get_missing_ratios_rows(x)
-print(L)
+z = pd.read_csv("titanic_train.csv", sep=",")
+x = z
+x = drop_missing_data_rows(z, .1)
+print x.shape
+
