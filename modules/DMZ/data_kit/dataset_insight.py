da737d38e36ec93c62b2976b259fe3f715129c91
Zak7062 <zak7062@gmail.com>
2016-11-05 11:38:40 -0500

    Updates to dataset_insight
    Created toggle.py in the utils folder and used it to clean up the
    get_missing_ratios function.
    Magic.

--- modules/DMZ/data_kit/dataset_insight.py
+++ modules/DMZ/data_kit/dataset_insight.py
@@ -1,7 +1,10 @@
 # This is for analyzing columns or datasets, to figure out how to best act upon them.
-
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
 import csv
 import pandas as pd
+from utils import toggle
 
 # This will take the target "predicted" column and decide if classification or regression should be used.
 def get_prediction_type(target_column):
@@ -35,23 +38,21 @@ def get_delimiter(path):
 # Gets the ratio of missing values to existing values in a dataframe. Either operates on rows or columns, depending
 # on input.
 def get_missing_ratios(pandas_data, method):
+    axis_toggle = toggle.get_axis_toggle(method)
 
-    if method == "column":
-        missing_data_counts = pandas_data.isnull().sum(axis=0)
-        shape_axis = 1
-        index_axis = 0
-    elif method == "row":
-        missing_data_counts = pandas_data.isnull().sum(axis=1)
-        shape_axis = 0
-        index_axis = 1
+    if axis_toggle != 2:
+
+        missing_data_counts = pandas_data.isnull().sum(axis_toggle)
+        ratios = []
+
+        for i in range(0, pandas_data.shape[not axis_toggle]):
+            ratio = float(missing_data_counts[i]) / pandas_data.shape[axis_toggle]
+            ratios.append(ratio)
+        return ratios
+
     else:
-        print("Please input row or column.")
+
         return pandas_data
-    ratios = []
-    for i in range(0, pandas_data.shape[shape_axis]):
-        ratio = float(missing_data_counts[i]) / pandas_data.shape[index_axis]
-        ratios.append(ratio)
-    return ratios
 
 
 # Takes a dataframe and returns the ratio of missing data for each column and the dtypes for each column.
