27550d8b7945e90b9f595a6e46f21c8b8f8c4088
asclines <injpk23@gmail.com>
2016-11-05 14:08:59 -0500

    Configuration: Added XML tag checking to make sure all required XML tags are in the passed in XML file.

--- modules/hub/configuration.py
+++ modules/hub/configuration.py
@@ -7,12 +7,32 @@ manipulating configuration information.
 import xml.etree.ElementTree as ET
 
 """
+The list below contains the values of the required XML tags that should be
+in the configuration XML file. If any one of these is not in the XML file, an
+error will be thrown.
+"""
+tag_root = "MLTF-Configuration"
+required_tags = ["Project-Name","User-Name","Firebase",\
+"Files","Prediction","Models"]
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class ConfigurationError(Exception):
+    """Generic error for exceptions created in this module."""
+    def __init__(self, message):
+        super(ConfigurationError,self).__init__(message)
+        self.message = message
+
+def _get_dict_from_xml(root):
+    """
     Recursively goes through the XML tree creating a dict object from the
     XML elements.
     Key = XML tag name
     Value = a dict of the child elements OR the text between the elements
-"""
-def _get_dict_from_xml(root):
+    """
     result = {}
     if len(list(root)) == 0:
         result[root.tag] = root.text.rstrip().strip()
@@ -31,10 +51,20 @@ class Configuration(object):
         self.config_file_name = config_file_name
         self.config_data = {}
         self._load_file()
+        self._validate_loaded_dict()
 
 
     def _load_file(self):
+        """ Loads the XML file and checks root tag for validaty"""
         tree = ET.parse(self.config_file_name)
         root = tree.getroot()
+        if not root.tag == tag_root:
+            raise ConfigurationError('Required XML root tag [{}] not found in {}'.format(tag_root,self.config_file_name))
+
         self.config_data = _get_dict_from_xml(root)
-        print(self.config_data)
+
+    def _validate_loaded_dict(self):
+        """ Checks to make sure all required tags are in the loaded dictionary from the XML file """
+        for tag in required_tags:
+            if not self.config_data.has_key(tag):
+                raise ConfigurationError('Required XML tag [{}] not found in {}'.format(tag,self.config_file_name))
