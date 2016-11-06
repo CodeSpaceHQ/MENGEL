aa4cabc512e6d9e508adb3d281818fa601df4a7f
Zakery Fyke <zak7062@gmail.com>
2016-11-06 13:06:32 -0600

    added column to get_composition
    get_composition uses missing_ratios, which can work either on rows or
    columns, and as far as I know we only want get_composition to get the
    ratios for columns.

--- modules/DMZ/data_kit/dataset_insight.py
+++ modules/DMZ/data_kit/dataset_insight.py
@@ -53,9 +53,8 @@ def get_missing_ratios(pandas_data, method):
     return pandas_data
 
 
-# Takes a dataframe and returns the ratio of missing data for each column and the dtypes for each column.
+# Takes a dataframe and returns the ratio of missing data for each and dtypes for each column.
 def get_composition(pandas_data):
-    missing_ratios = get_missing_ratios(pandas_data)
     column_types = pandas_data.dtypes
     return missing_ratios, column_types
 
