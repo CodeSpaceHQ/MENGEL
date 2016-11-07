97d4e29fffac20e4db042fdf26a59c92039d6eb8
Zak7062 <zak7062@gmail.com>
2016-11-06 22:07:12 -0600

    Removed parts of the FillerStrategy class, Col drop update
    Since we weren't really using the column types from get_composition, I
    did away with using it entirely. I've left get_composition in
    dataset_insight in case we need it at  some point, but since getting the
    dtypes is so trivial, I did away with it.
    
    Also updated how the column drop works in situations where the feature
    needs to be dropped. Previously the column dropped would be filled with
    "None", now it is irradicated entirely.

--- modules/DMZ/data_kit/filler_strategy.py
+++ modules/DMZ/data_kit/filler_strategy.py
@@ -6,26 +6,21 @@ class FillerStrategy(object):
 
     def __init__(self):
         self.file_name = None
-        self.pandas_dataset = None
-        self.missing_ratios_dict = None
         self.avg_thresh = None
         self.value_thresh = None
-        self.drop_thresh = None
         self.fill_value = None
-        self.column_types = None
+        self.pandas_dataset = None
+        self.missing_ratios_dict = None
 
         self.get_config_options()
         self.get_dataset()
-        self.get_dataset_composition()
-        #self.get_missing_ratios()
+        self.get_missing_ratios_dict()
         self.run_fillers()
-
 
     def get_config_options(self):
         # self.file_name = raw_input("Please input the file name: ")
         # self.avg_thresh = raw_input("Please input the avg threshold: ")
         # self.value_thresh = raw_input("Please input the value threshold: ")
-        # self.drop_thresh = raw_input("Please input the drop threshold: ")
         self.file_name = "titanic_train.csv"
         self.avg_thresh = .50
         self.value_thresh = .80
@@ -34,13 +29,9 @@ class FillerStrategy(object):
     def get_dataset(self):
         self.pandas_dataset = pd.read_csv(self.file_name, sep=None)
 
-    def get_dataset_composition(self):
-        missing_ratios, self.column_types = dataset_insight.get_composition(self.pandas_dataset)
+    def get_missing_ratios_dict(self):
+        missing_ratios = dataset_insight.get_missing_ratios(self.pandas_dataset, "column")
         self.missing_ratios_dict = dict(zip(self.pandas_dataset.columns.values, missing_ratios))
-
-    # def get_missing_ratios(self):
-    #     missing_ratios = dataset_insight.get_missing_ratios(self.pandas_dataset, "column")
-    #     self.missing_ratios_dict = dict(zip(self.pandas_dataset.columns.values, missing_ratios))
 
     def run_fillers(self):
 
@@ -52,10 +43,6 @@ class FillerStrategy(object):
                     elif self.missing_ratios_dict[col] <= self.value_thresh:
                         self.pandas_dataset[col] = data_filler.fill_missing_data(self.pandas_dataset[col], self.fill_value)
                     else:
-                        self.pandas_dataset[col] = self.pandas_dataset.drop(col, axis = 1, inplace = True)
+                        self.pandas_dataset.drop(col, axis = 1, inplace = True)
             # We'd eventually like to run data fillers on all nonstring values and use their filled values to
             # predict what should fill missing string data, but that's an icebox goal.
-
-
-x = FillerStrategy()
-print x.pandas_dataset
