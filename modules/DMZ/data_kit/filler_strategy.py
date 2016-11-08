f0c1fa1c5141908de70cb69997d5735fd02daf78
Zakery Fyke <zak7062@gmail.com>
2016-11-08 10:47:36 -0600

    format change to filler_strategy

--- modules/DMZ/data_kit/filler_strategy.py
+++ modules/DMZ/data_kit/filler_strategy.py
@@ -41,5 +41,4 @@ class FillerStrategy(object):
                     elif self.missing_ratios_dict[col] <= self.value_thresh:
                         self.pandas_dataset[col] = data_filler.fill_missing_data(self.pandas_dataset[col], self.fill_value)
                     else:
-                        self.pandas_dataset.drop(col, axis = 1, inplace = True)
-
+                        self.pandas_dataset.drop(col, axis = 1, inplace=True)
