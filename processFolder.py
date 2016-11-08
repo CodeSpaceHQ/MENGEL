8a0c8cc8ec9d58ec6f1d377f8c5caeac7b9ece69
telelu03 <t.elelu03@gmail.com>
2016-11-08 10:03:16 -0600

    Add files via upload
    Slightly changing the proccessFolder() module, wanting to take the edited images and use the cvImage Library to convert them into numPy arrays.
    
    Later on, the arrays will be put into a file, presuming a csv file, that can be fed into other parts of the project.

--- processFolder.py
+++ processFolder.py
@@ -15,29 +15,39 @@
 from PIL import Image # image libraries and operations
 import os, sys        # directories 
 import imghdr         # image file recognition
+import numpy as np    # numpy arrays
+import cv2
 
 # Sample Paths
 # /mnt/c/Users/Owner/Pictures/SamplePictures/
-# /mnt/c/Users/Owner/Documents/SE2CS4365/TestImages/
+# /mnt/c/Users/Tayo/Documents/SE2CS4365/TestImages/
 
 path = raw_input("Enter the name of the directory you want to manipulate. \n")
 dirs = os.listdir(path) # store path
 print("Start.")
 
-#list of image types
+#list of image types 
 imgfiles = ['rgb','gif','pbm','pgm','ppm','tiff','rast','xbm','jpeg','jpg','bmp','png','webp','exr']
+i = 0
+imgArray = []
+
 
 def resizeFolder():
     for item in dirs:
         print("Converting: " + item)
         if imghdr.what(path+item) in imgfiles:       #if the file has the correct type
             print("Opening image...")
-            img = Image.open(path+item).convert('L') #open and convert to greyscale
+            img = Image.open(path+item) #.convert('L') #open and convert to greyscale
             a,b = os.path.splitext(path+item)
-
             newImg = img.resize((75,75), Image.ANTIALIAS) #resize and keep quality
             newImg.save(a + 'edit.jpg', 'JPEG', quality = 90) #save the image with edit appended to it
+
+            imgNumPy = cv2.imread(a + 'edit.jpg') # convert edited images to numpy array
+            imgArray[i] = imgNumPy                # add to array
+            i = i+1
+            
             print("New image saved: " + item + "\n")
+            print("Image array: " + imgNumPy)
 
     print("Done!")
 
