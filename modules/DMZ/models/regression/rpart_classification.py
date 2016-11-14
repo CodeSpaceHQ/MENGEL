44bde76c0c96b3cacaf04751a4608a577af2c049
Ryan Berg <ryanmcberg@gmail.com>
2016-11-14 12:46:49 -0600

    Updated rpart fit to not actually forward to an R file

--- modules/DMZ/models/regression/rpart_classification.py
+++ modules/DMZ/models/regression/rpart_classification.py
@@ -3,50 +3,67 @@ Ryan Berg 10/19/16
 """
 
 import sys
-import subprocess
-import os
 import pandas as pd
-from rpy2.robjects import DataFrame, Formula, pandas2ri, importr
+from rpy2.robjects import DataFrame, Formula, pandas2ri
+from rpy2.robjects.packages import  importr
 pandas2ri.activate()
-rparty = importr('rpart')
-proc = importr("pRoc")
 
-scriptpath = os.path.dirname(os.path.realpath(__file__)) + "/r_classification.R"
-scorescript = os.path.dirname(os.path.realpath(__file__)) + "/r_rpartpredict.R"
+# Imports for the R libraries needed
+rparty = importr('rpart')
+proc = importr("pROC")
+base = importr("base")
+stats = importr("stats")
+caret = importr("caret")
 
 class Rpart:
 
     def __init__(self):
         self.model = pd.DataFrame
         self.score = pd.DataFrame
-
-    #Needed to send R data it can work with rather than a pandas dataframe.
-    
+        self.outcome = ""
 
     #Returns a probability matrix, based on data results.
-    def score(self, trained_results, actual_data):
+    def score(self):
         return self.score
 
     #Trains the model.
-    def fit(self, test_data,  target):
-        test_d = [self.convert_to_rdata(test_data)]
-        target = [target]
-        cmd = ["Rscript", scriptpath] + test_d + target
-        subprocess.call(cmd)
+    def fit(self, training_data,  target):
+        """
+        :param training_data: a pandas dataframe.
+        :param target: a string referring to the target varialbe to be predicted
+        :return: An rpart model
+        """
+
+        self.outcome = target
+        #Converting to proper format for R functions
+        train_data = DataFrame(training_data)
+        formula = target + " ~ ."
+
+        #TODO: HyperParameter Incorporateion
+
+        model = rparty.rpart(formula = formula,
+                             data = train_data,
+                             method = "class"
+                             #control = rparty.rpart_control(minsplit = ?, cp = ?)
+                             )
+
+        self.model = model
+        return model
 
     #Uses a model to make predictions on a dataset.
     def predict(self, model, dataset):
-        command = ["Rscript", scorescript] + [model] + [dataset]
-        self.score = subprocess.call(command)
-
+        pred = stats.predict(model, dataset, type = "class")
+        print(base.table(pred, dataset[self.outcome]))
+        return
 
 if __name__ == "__main__":
-    test_data = sys.argv[1]
-    #train_data = sys.argv[2]
+    train_data = sys.argv[1]
     predictor = sys.argv[2]
+
+    train_data = pd.read_csv(train_data, delimiter= ";")
+    train_data = pd.DataFrame(train_data)
+
     rpart = Rpart()
-    train_data_fun = DataFrame(pd.read_csv(test_data))
-    formula = Formula(predictor + " ~ .")
-    model = rparty.rpart(formula = formula, data = train_data_fun)
-    print(model)
-    #rpart.fit(train_data_fun, predictor)
+    rpart.fit(train_data, predictor)
+    rpart.predict(rpart.model, train_data)
+    print(rpart.model)
