ec218b7c9bca101de3847fa7421e7d775ca190ec
Tayo <t.elelu03@gmail.com>
2016-11-27 02:53:27 -0600

    Changed variable and method names to conform with style guides, and changed logic of get_image_array to do away with size requirements, as they don't make sense for that method

--- modules/DMZ/data_kit/process_folder.py
+++ modules/DMZ/data_kit/process_folder.py
@@ -1,7 +1,6 @@
-# Import needed libraries
-from PIL import Image  # image libraries and operations
-import os, sys  # directory libraries
-import imghdr  # image file recognition
+from PIL import Image  # image operations
+import os     # operations on directories
+import imghdr # image file recognition
 import numpy  # numpy arrays
 
 # Sample Paths
@@ -11,35 +10,33 @@ import numpy  # numpy arrays
 # list of image types
 imgfiles = ['rgb', 'gif', 'pbm', 'pgm', 'ppm', 'tiff', 'rast', 'xbm', 'jpeg', 'jpg', 'bmp', 'png', 'webp', 'exr']
 
-
 # Splitting up this module into smaller ones
 
 # Given a filepath and size, resizeFolder will enter the folder
 # and edit the files to be square, with lxw as sizexsize
-def resizeFolder(path, size):
+def resize_folder(path, size):
     dirs = os.listdir(path)  # store the path.
 
     for item in dirs:
         if imghdr.what(path + item) in imgfiles:  # if the file has the correct type
-            img = Image.open(path + item)  # .convert('L') #open and convert to greyscale
-            a, b = os.path.splitext(path + item)
-            newImg = img.resize((size, size), Image.ANTIALIAS)  # resize and keep quality
-            newImg.save(a + 'edit.jpg', 'JPEG', quality=90)  # save the image with edit appended to it
+            img = Image.open(path + item)         # open the image
+            strpath,ext = os.path.splitext(path + item)  #store the filepath in a string in strpath
+            new_img = img.resize((size, size), Image.ANTIALIAS)  # resize and keep quality
+            print(strpath)
+            new_img.save(strpath + 'edit.jpg', 'JPEG', quality=90)  # save the image with edit appended to it
 
 
-# given a directory string, will return the numpyarray of square images with lxw = size x size
-def getImageArray(path, size):
-    imgArray = []
-    width = 0
-    height = 0
+# given a directory string, will return the numpyarray of square images that have been previously edited
+def get_image_array(path):
+    img_array = []
     dirs = os.listdir(path)  # store the path.
 
     for item in dirs:
         if imghdr.what(path + item) in imgfiles:  # if the file has the correct type
-            newImg = Image.open(path + item)
-            width, height = newImg.size
-            if width == size and height == size:
-                imgNumPy = numpy.array(newImg)  # convert edited images to numpy array
-                imgArray.append(imgNumPy)  # add to array
+            new_img = Image.open(path + item)
+            strpath, ext = os.path.splitext(path + item)  #store the filepath in a string in strpath
+            if strpath[-4:] == 'edit':                   #if the image has been previously edited
+                img_numpy = numpy.array(new_img)  # convert edited images to numpy array
+                img_array.append(img_numpy)  # add to array
 
-    return imgArray
+    return img_array
