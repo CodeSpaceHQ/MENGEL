2778582b0ced7fdc05858f36e93b15760274c291
Tayo <t.elelu03@gmail.com>
2016-12-07 18:32:07 -0600

    Additional modules get_image_csv and get_image_collection added. get_image_csv not working, as it is on hold

--- modules/DMZ/data_kit/process_folder.py
+++ modules/DMZ/data_kit/process_folder.py
@@ -2,6 +2,8 @@ from PIL import Image  # image operations
 import os     # operations on directories
 import imghdr # image file recognition
 import numpy  # numpy arrays
+import matplotlib #img collection library
+matplotlib.use('Agg')
 
 # Sample Paths
 # /mnt/c/Users/Owner/Pictures/SamplePictures/
@@ -40,3 +42,43 @@ def get_image_array(path):
                 img_array.append(img_numpy)  # add to array
 
     return img_array
+
+# given an array of numpyarrays of images, this function will return a csv file representing the info
+# each row into the csv represents all info for an image
+# each column represents pixel information at a location, there will be (size + 1)
+# number of columns in (including an ID column)
+# each entry into a column will be a 3-tuple array with the RGB info for the pixel at that location
+def get_image_csv(img_array, size)
+
+    for image in img_array
+        print("This is image: " + image)
+        #write headers?
+        #write to ID Column?
+        img_row = 0
+
+        row_array = []  #reset the row array to empty
+
+        #nested for loop over the numpy array of image to access pixel info
+        for row in range(0,size)
+            for col in range(0,size)
+                # get info of pixel at that space
+                # append to row array
+                row_array.append() #append that pixel entry to the row array
+
+        #wrote row array to row of csv file
+
+# given a directory, this will build a string of all the paths to the image,
+# and add them to an image collection. to be used to feed into a classifier
+# from matplotlib
+def build_image_collection(path):
+    path_array = []
+    dirs = os.listdir(path)  # store the path.
+
+    for item in dirs:
+        if imghdr.what(path + item) in imgfiles:  # if the file has the correct type
+            str_path, ext = os.path.splitext(path+item)  #store the filepath in a string in strpath
+            if str_path[-4:] == 'edit':     # if the image has been previously edited
+                path_array.append(str_path + ext)  # add to path array
+
+    image_col = ImageCollection(path_array)
+    return image_col
