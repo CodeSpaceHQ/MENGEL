d8c2b54af0e2ab65b787625b2c52a1d7a7e4908f
Tayo <t.elelu03@gmail.com>
2016-12-12 14:09:08 -0600

    Removed testing within my digit_recognition file and made a seperate file with tests. Also removed my path that I've been using on my local machine and used Issac's setup.py module for pathing, cleaned up the code, and removed unnecessary comments

--- modules/DMZ/models/classification/digit_recognition.py
+++ modules/DMZ/models/classification/digit_recognition.py
@@ -2,7 +2,7 @@
 
 # Standard scientific Python imports
 import matplotlib
-matplotlib.use('Agg')   #Force matplotlib to not use any Xwindows backend (causes errors)
+matplotlib.use('Agg')   # Force matplotlib to not use any Xwindows backend (causes errors)
 from sklearn import svm # Import svm classifier
 import skimage.io       # reading images
 import os     # directories
@@ -28,13 +28,13 @@ class digit_recognition:
         for item in dirs:
             if imghdr.what(path + item) in imgfiles:    # if the file has the correct type
                 str_path, ext = os.path.splitext(path + item)  # store the filepath in a string in strpath
-                img_array.append(skimage.io.imread(str_path + ext)) #add image to img_array
+                img_array.append(skimage.io.imread(str_path + ext)) # add image to img_array
                 target_str = str_path[path_length:]  # build the target array to correspond with image collection
                 target_str = target_str[:7]
-                target_int = int(target_str[-1:]) #get the number that the img represents from the filename
-                target_array.append(target_int) #append to target_array
+                target_int = int(target_str[-1:]) # get the number that the img represents from the filename
+                target_array.append(target_int) # append to target_array
 
-        #Build the matrix of rows x features
+        # Build the matrix of rows x features
         n_samples = len(img_array)
         features = 28 * 28  # size of image
         row_array = []
@@ -44,20 +44,18 @@ class digit_recognition:
             row_array = numpy.reshape(member, (features))
             matrix.append(row_array)
 
-        #randomize the data
+        # randomize the data
         a = tuple(matrix)
         b = tuple(target_array)
         zip_list = list(zip(a,b))      # tie each entry in the matrix to its prediction value
         numpy.random.shuffle(zip_list) # randomize
 
-        train_int = int(0.7 * n_samples)  #amt of data for training
-        test_int = n_samples - train_int  #amt of data for testing
+        train_int = int(0.7 * n_samples)  # amt of data for training
+        test_int = n_samples - train_int  # amt of data for testing
 
-        digit_recognition.train = zip_list[:test_int] #70% of the data stored in train_data
-        digit_recognition.test  = zip_list[-test_int:] #30% of the data stored in test_data
-
-        #return a tuple of zipped lists
-        #return train_data, test_data
+        # Set Global variables of testing and training data to our initialized data
+        digit_recognition.train = zip_list[:test_int]  # 70% of the data stored in train_data
+        digit_recognition.test  = zip_list[-test_int:] # 30% of the data stored in test_data
 
 
     # Given a data in the form of a tuple of two zipped lists (each image is paired with its classification)
@@ -78,22 +76,12 @@ class digit_recognition:
 
         # Get the prediction list using the testing data
         matrix_test_tup, digits_test_tup = zip(*test_data)  # unzip the randomized testing list
-        matrix_test = list(matrix_test_tup)
-        digits_test = list(digits_test_tup)
+        matrix_test = list(matrix_test_tup) # testing data of images
+        digits_test = list(digits_test_tup) # testing data for digits/labels
 
-        #get expected and predicted values in list form
+        # get expected and predicted values in list form
         expected = digits_test
         predicted = list(classifier.predict(matrix_test))
 
-        #return a tuple of two lists
+        # return a tuple of two lists
         return predicted, expected
-
-#Testing
-path = "/mnt/c/Users/Tayo/Documents/SE2CS4365/HandWrittenChar/eggChar/mnist_jpgfiles/trainsmall/"
-recognizer = digit_recognition(path)
-pred, exp  = recognizer.predict_images()
-
-print("Last 20 predictions: ")
-print(pred[-20:])
-print("Last 20 expected values:")
-print(exp[-20:])
