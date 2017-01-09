d1cc2c2c354b70bb1957d46c27e297e1a1e7ca79
Isaac Griswold-Steiner <isaac31415926@gmail.com>
2017-01-09 11:34:45 -0600

    travis

--- tests/unittest_data/test_file_downloader.py
+++ tests/unittest_data/test_file_downloader.py
@@ -1,4 +1,4 @@
 import urllib
 
 testfile = urllib.URLopener()
-testfile.retrieve("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", "winequality-red.csv")
+testfile.retrieve("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", "tests/unittest_data/winequality-red.csv")
